611b466628ce6d659cb6122fff4213eb7ff1d695
Andrey <alvice01andru@gmail.com>
2022-10-30 12:22:13 +0300

    Рефакторинг верстки блока reviews

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -3,7 +3,7 @@ import fonter from 'gulp-fonter';
 export const convertFonts = () => {
    return app.gulp.src(`${app.path.src.fonts}/*.otf`)
       .pipe(fonter({
-         formats: ['woff', 'ttf', 'otf']
+         formats: ['woff', 'ttf']
       }))
       .pipe(app.gulp.dest(app.path.build.fonts));
 }
